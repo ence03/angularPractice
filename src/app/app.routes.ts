6dc2f9ae09ca6b8f3a982394bacb19e3af5dbfb4
Ence <justclarex.03@gmail.com>
2025-02-04 16:36:51 +0800

    cart component added, navigation to cart using router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FoodComponent } from './food/food.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'food', component: FoodComponent},
-    {path: '', redirectTo: "/home", pathMatch: 'full'},
-]; 
+  { path: 'home', component: HomeComponent },
+  { path: 'food', component: FoodComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
